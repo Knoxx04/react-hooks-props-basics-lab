e6694ccb8b0c64c9f59a0de3a1aab2a918ac2a76
Knoxx04 <onyiknoxx3@gmail.com>
2023-01-12 21:08:14 +0300

    completed optional

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {} from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
@@ -7,12 +7,14 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
+
+
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
